30f17db2508ed49317e8a4fefa14b386477de1ab
Derek <derek.hou@alumni.utoronto.ca>
2021-02-20 03:12:41 -0500

    Added useEvent window event call to allow text selection starting from the text area and having the mouseup on the window.

--- src/components/wysiwyg/Wysiwyg.js
+++ src/components/wysiwyg/Wysiwyg.js
@@ -1,24 +1,45 @@
 import React, { useState, setState, useEffect, useContext } from 'react';
 import { GlobalContext } from '../../context/GlobalState';
+import { useEvent } from '../useEvent';
 
-
+/**
+ * Instead of only using a mouse event on the textarea element for getting the selection,
+ * I am using a window event for when a user leaves the textarea on the mouse up.
+ */
 export const Wysiwyg = () => {
-    //const { italicize } = useContext(GlobalContext);
-    //const { getSelectedPositions } = useContext(GlobalContext);
-
     var selectedTextStart;
     var selectedTextEnd;
     var selectedText;
-    //let [selectedText, getSelectedPositions] = useState('');
-    /* let [italicText, italicize] = useState('');
-    let [boldText, bold] = useState(''); */
+
     let [textAreaText, setTextAreaText] = useState('');
     let [historyArr, setHistory] = useState([]);
-    
+
     const handleChange = (e) => {
         setTextAreaText(e.target.value);
     };
     
+    const getSelected = (e) => { 
+        console.log(e.target.name);
+        if(e.target.name === 'wysiwyg') {
+            selectedTextStart = e.target.selectionStart;
+            selectedTextEnd = e.target.selectionEnd;
+
+            selectedText = e.target.value.substring(selectedTextStart, selectedTextEnd);
+            if(selectedText.length <= 0) {
+                return; // stop here if selection length is <= 0
+            }
+            
+            // log the selection
+            console.log("startPos: " + selectedTextStart, " | endPos: " + selectedTextEnd );
+            console.log("selectedText: " +  selectedText);
+        } else {
+            selectedText = e.view.getSelection().toString();
+            //console.log(e.view.getSelection().toString());
+        }
+    }
+
+    
+
     setHistory = (str, tag) => {
         historyArr = addHistory(str, tag);
         //console.log(historyArr);
@@ -37,7 +58,6 @@ export const Wysiwyg = () => {
 
     let addHtmlTag = (str, tag) => {
         let rtnStr;
-
         if(tag === '') {
             rtnStr = str;
         } else if(tag === 'ul' || tag === 'ol') {
@@ -48,29 +68,15 @@ export const Wysiwyg = () => {
                 rtnStr = `<${tag}>`;
                 strArr.map(item => rtnStr += `<li>${item}</li>`);
                 rtnStr += `</${tag}>`;
+            } else {
+                rtnStr = `<${tag}><li>${str}</li></${tag}>`;
             }
         } else {
             rtnStr = `<${tag}>${str}</${tag}>`;
         }
 
         return rtnStr;
-    }
-
-    let getSelectedPositions = (e) => {
-        
-        selectedTextStart = e.target.selectionStart;
-        selectedTextEnd = e.target.selectionEnd;
-        
-        selectedText = e.target.value.substring(selectedTextStart, selectedTextEnd);
-        if(selectedText.length <= 0) {
-            return; // stop here if selection length is <= 0
-        }
-        
-        // log the selection
-        console.log("startPos: " + selectedTextStart, " | endPos: " + selectedTextEnd );
-        console.log("selectedText: " + selectedText);
-
-    }
+    } 
 
     let getShortcut = (e) => {
         console.log(e)
@@ -78,14 +84,14 @@ export const Wysiwyg = () => {
 
     let updateTextArea = (str) => {
         try {        
-            if(selectedText.length > 0 && selectedTextEnd > 0) {
+            if(selectedText.length > 0) {
                 textAreaText = textAreaText.substr(0, selectedTextStart) + str + textAreaText.substr(selectedTextEnd, textAreaText.length);
                 setTextAreaText(textAreaText);
             } else {
                 throw new Error('selectedText is empty');
             }
         } catch(err) {
-            console.error(err)
+            console.error(err);
         }
     } 
 
@@ -134,12 +140,15 @@ export const Wysiwyg = () => {
         }
     }
 
+    // window events
+    useEvent('mouseup', getSelected);
+
     return (
         <>
             <button onClick={italicize}>i</button>
             <button onClick={bold}>b</button>
             <button onClick={unorderedList}>ul</button>
-            <textarea className="editor" onMouseUp={getSelectedPositions} onKeyUpCapture={getShortcut} value={textAreaText} onChange={handleChange}></textarea>
+            <textarea className="wysiwyg-editor" name="wysiwyg" value={textAreaText} onChange={handleChange}></textarea>
             <div dangerouslySetInnerHTML={{__html: textAreaText}} />
             <ul>
                 { historyArr.map((item, index) => 
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -5,8 +5,7 @@ import AppReducer from './AppReducer';
 const initialState = {
     selectedText: '',
     selectedTextStart: '',
-    selectedTextEnd: '',
-    interpolatedText: 'asdf'
+    selectedTextEnd: ''
 }
 
 // create context - bring this into other files and components to use it
@@ -16,32 +15,10 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => { // children is destructured
     const [state, dispatch] = useReducer(AppReducer, initialState); // reducer uses dispatch
 
-    // get the selected positions within the text area and set it to the state
-    const getSelectedPositions = (event) => {
-        state.selectedTextStart = event.target.selectionStart;
-        state.selectedTextEnd = event.target.selectionEnd;
-
-        state.selectedText = event.target.value.substring(state.selectedTextStart, state.selectedTextEnd);
-        if(state.selectedText.length <= 0) {
-            return; // stop here if selection length is <= 0
-        }
-        
-        // log the selection
-        console.log("startPos: " + state.selectedTextStart, " | endPos: " + state.selectedTextEnd );
-        console.log("selectedText: " +  state.selectedText);
-
-    }
-
-    /* const italicize = () => {
-        console.log("startPos: " + state.selectedTextStart, " | endPos: " + state.selectedTextEnd );
-        console.log("selectedText: " +  state.selectedText);
-    } */
-
     // value property uses an object
     return (
     <GlobalContext.Provider value={{ // allows for tracks to be passed to deeper components        
-        getSelectedPositions,
-        selectedText: state.selectedText
+        
     }}>
     {children}
     </GlobalContext.Provider>);
